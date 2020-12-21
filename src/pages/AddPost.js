dab1a31390cff1f01eb12b9f959a4deca0b29bf3
volidenko <volidenko@.i.ua>
2020-12-21 23:55:39 +0200

    comm

--- src/pages/AddPost.js
+++ src/pages/AddPost.js
@@ -4,26 +4,47 @@ export default function AddPost() {
     const [id, setId]=useState(1);
     const [title,setTitle]=useState("");
     const [author, setAuthor]=useState("");
-    const [id, setId]=useState("");
-
-
+    const [text, setText] = useState("");
+    const handleSubmit = e => {
+        e.preventDefault();
+        addPost({id, title, author, text});
+    }
+    const handleChange = e => {
+        switch (e.target.name) {
+            case "id":
+                setId(e.target.value);
+                break;
+            case "title":
+                setTitle(e.target.value);
+                break;
+            case "author":
+                setAuthor(e.target.value);
+                break;
+            case "text":
+                setText(e.target.value);
+                break;
+            default:
+        }
+    }
     return (
-        <form onSubmit={ }>
+        <form onSubmit={handleSubmit}>
             <div className="form-group">
                 <label>Id</label>
-                <input type="number" value={ } className="form-control" name="id"></input>
+                <input type="number" value={id} onChange={handleChange} className="form-control" name="id"></input>
             </div>
             <div className="form-group">
                 <label>Title</label>
-                <input type="text" value={ } className="form-control" name="title"></input>
-            </div>            <div className="form-group">
-                <label>Author</label>
-                <input type="text" value={ } className="form-control" name="author"></input>
-            </div>            <div className="form-group">
-                <label>Text</label>
-                <input type="text" value={ } className="form-control" name="text"></input>
+                <input type="text" value={title} onChange={handleChange} className="form-control" name="title"></input>
             </div>
-            <input type="submit" className="btn-btn-primary"></input>
+            <div className="form-group">
+                <label>Author</label>
+                <input type="text" value={author} onChange={handleChange} className="form-control" name="author"></input>
+            </div>
+            <div className="form-group">
+                <label>Text</label>
+                <input type="text" value={text} onChange={handleChange} className="form-control" name="text"></input>
+            </div>
+            <input type="submit" className="btn btn-primary"></input>
         </form>
     )
 }
--- src/pages/NavMenu.js
+++ src/pages/NavMenu.js
@@ -2,13 +2,12 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 export default function NavMenu() {
+    let active= {color: "red"};
     return (
         <nav class="navbar navbar-light bg-light">
             <NavLink to="/" activeStyle={active}>Main</NavLink>
-            <NavLink to="/posts">Posts</NavLink>
-
-            <NavLink to="/posts">Posts</NavLink>
+            <NavLink to="/add" activeStyle={active}>New Post</NavLink>
+            <NavLink to="/posts" activeStyle={active}>Posts</NavLink>
         </nav>
     )
-
 }
